9a78e8bea6b5ce93534437acab1d020e80024fc3
Bruno <brunomaiamorais@gmail.com>
2021-04-16 22:23:00 -0300

    Initial Commit

---
+++ FetchTweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FetchTweets
+//
+//  Created by Bruno Maia de Morais on 16/04/21.
+//  Copyright © 2021 Bruno Maia de Morais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
